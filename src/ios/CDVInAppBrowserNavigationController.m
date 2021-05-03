7f0070c172a57f05efb6a45745afb11bd45c477a
Matt Morgan <m@getplatypi.com>
2021-05-03 13:34:59 -0400

    removing unsupported function

--- src/ios/CDVInAppBrowserNavigationController.m
+++ src/ios/CDVInAppBrowserNavigationController.m
@@ -74,14 +74,5 @@
     return 1 << UIInterfaceOrientationPortrait;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    if ((self.orientationDelegate != nil) && [self.orientationDelegate respondsToSelector:@selector(shouldAutorotateToInterfaceOrientation:)]) {
-        return [self.orientationDelegate shouldAutorotateToInterfaceOrientation:interfaceOrientation];
-    }
-
-    return YES;
-}
-
 
 @end
